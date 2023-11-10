371d9fc2a154b151d7b41e26a36a87d176ed6b39
Julio Cesar <141031457+raikxyz@users.noreply.github.com>
2023-11-10 11:21:31 -0300

    Update App.js
    I've added persistent login and a few more features

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
 import "./App.css";
-import { db } from "./firebase.js";
+import { db, auth } from "./firebase.js";
 import { useEffect, useState } from "react";
-import Header from './Header.js';
+import Header from "./Header.js";
+import Post from "./Post.js";
 
 function App() {
   const [user, setUser] = useState();
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
+    const unsubscribeAuth = auth.onAuthStateChanged(function (val) {
+      setUser(val?.displayName);
+    });
 
+    const unsubscribePosts = db
+      .collection("posts")
+      .orderBy("timeStamp", "desc")
+      .onSnapshot(function (snapshot) {
+        setPosts(
+          snapshot.docs.map(function (document) {
+            return { id: document.id, info: document.data() };
+          })
+        );
+      });
+
+    return () => {
+      unsubscribeAuth();
+      unsubscribePosts();
+    };
   }, []);
 
   return (
     <div className="App">
       <Header setUser={setUser} user={user} />
+      {posts.map(function (val) {
+        return <Post user={user} key={val.id} info={val.info} id={val.id} />;
+      })}
     </div>
   );
 }
